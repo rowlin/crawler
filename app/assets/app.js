011237a5d6a368c984fde52c0991d598888b26db
alice <rowlinest90@gmail.com>
2022-06-07 15:33:08 +0300

    fix front (add bulma)

--- app/assets/app.js
+++ app/assets/app.js
@@ -5,6 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import '../node_modules/bulma/bulma.sass'
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
